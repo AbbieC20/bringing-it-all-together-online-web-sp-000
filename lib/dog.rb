ad6d38cde12fa00baa08b5e98a68b6b04c2fafc9
remote-torrent-6747 <abbyc@sky.com>
2021-03-20 16:40:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -13,10 +13,10 @@ class Dog
         id INTEGER PRIMARY KEY,
         name TEXT,
         breed TEXT
-    )
-    SQL
-  DB[:conn].execute(sql)
-end
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
 
 
 end
