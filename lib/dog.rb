126a2492bc1af1f16875690a4e26bbf9b664487e
remote-torrent-6747 <abbyc@sky.com>
2021-03-20 16:31:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,9 @@
 class Dog
   attr_accessor :id, :name, :breed
 
-  def initialize(name:, breed:, id: = nil)
+  def initialize(name:, breed:)
     @name = name
     @breed = breed
-    @id = id
   end
 
 end
