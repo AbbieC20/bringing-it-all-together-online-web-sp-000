9ff221690224ed25c812f673f07781eafd807983
remote-torrent-6747 <abbyc@sky.com>
2021-03-20 17:29:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -81,6 +81,11 @@ class Dog
     end.first
   end
 
+  def update
+    sql = "UPDATE students SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)  
+  end
+
 
 
 end
